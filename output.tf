a8a3ad1de8fe8ef9bd1846120f41887b1416a555
Venkat <psai96526@gmail.com>
2026-05-14 15:27:26 +0530

    up

--- output.tf
+++ output.tf
@@ -2,3 +2,22 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
+output "az_names" {
+    value = module.vpc.az_names
+}
+
+output "public_sunnet_ids" {
+  value = module.vpc.public_subnet_ids
+}
+
+output "private_sunnet_ids" {
+  value = module.vpc.private_subnet_ids
+}
+
+output "database_sunnet_ids" {
+  value = module.vpc.database_subnet_ids
+}
+
+
+
+
